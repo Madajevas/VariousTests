9d3aa0f5f6877b44774f91fd97feade5811c3263
Tomas Madajevas <tomas.madajevas@live.com>
2023-06-12 21:05:06 +0300

    add CompositeQueryable

---
+++ Various/Collections/Composite/CompositeQueryable/CompositeQueryProvider.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Various.Collections.Composite.CompositeQueryable
+{
+    internal class CompositeQueryProvider<T> : IQueryProvider
+    {
+        private readonly IQueryable<T>[] innerQueries;
+
+        public CompositeQueryProvider(IQueryable<T>[] innerQueries)
+        {
+            this.innerQueries = innerQueries;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            var queries = GetQueryables<TElement>(expression).ToArray();
+
+            return new CompositeQueryable<TElement>(queries);
+        }
+
+        private IEnumerable<IQueryable<TElement>> GetQueryables<TElement>(Expression expression)
+        {
+            // TODO: other types?
+            var methodCallExpression = expression as MethodCallExpression;
+            var arguments = methodCallExpression.Arguments;
+
+            IEnumerable<Expression> GetArguments(IQueryable<T> inner)
+            {
+                for (var i = 0; i < arguments.Count; i++)
+                {
+                    var arg = arguments[i];
+
+                    if (arg is ConstantExpression constExpr && constExpr.Type == typeof(CompositeQueryable<T>))
+                    {
+                        yield return Expression.Constant(inner);
+                        continue;
+                    }
+
+                    yield return arg;
+                }
+            }
+
+            foreach (var query in innerQueries)
+            {
+                var argsDebug = GetArguments(query).ToList();
+                var body = Expression.Call(null, methodCallExpression.Method, argsDebug);
+
+                var newQueryable = (IQueryable<TElement>)query.Provider.Execute(body);
+
+                yield return newQueryable;
+            }
+        }
+
+        public object? Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VariousTests/Collections/Composite/CompositeQueryable/CompositeQueryableTests.cs
@@ -0,0 +1,73 @@
+﻿using MockQueryable.NSubstitute;
+
+using Various.Collections.Composite.CompositeQueryable;
+
+namespace VariousTests.Collections.Composite.CompositeQueryable
+{
+    public class CompositeQueryableTests
+    {
+        [Test]
+        public void ProvidedWithTwoCollections_AppliesFilteringToBoth()
+        {
+            var q1 = new[] { new DummyEntity { Id = 0 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 1 }, new DummyEntity { Id = 2 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var list = composable
+                .Where(i => i.Id > 0)
+                .Where(i => i.Id < 2)
+                .ToList();
+
+            Assert.That(list, Is.All.Matches<DummyEntity>(e => e.Id == 1));
+        }
+
+        [Test]
+        public void ProvidedWithTwoCollections_CanRetrievePropertiesOfCollectionElements()
+        {
+            var q1 = new[] { new DummyEntity { Id = 0 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 1 }, new DummyEntity { Id = 2 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var list = composable
+                .Select(i => i.Id)
+                .ToList();
+
+            Assert.That(list, Is.EqualTo(new[] { 0, 1, 1, 2 }));
+        }
+
+        [Test]
+        public void ProvidedWithCollectionsContainingDuplicates_CanFilterThoseOut()
+        {
+            var q1 = new[] { new DummyEntity { Id = 0 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 1 }, new DummyEntity { Id = 2 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var list = composable
+                .Select(i => i.Id)
+                .Distinct()
+                .ToList();
+
+            Assert.That(list, Is.EqualTo(new[] { 0, 1, 2 })); // nope. only works on single underlying collection
+        }
+
+        [Test]
+        public void ProvidedWithUnorderedItemsInBothCollections_CanOrderThem()
+        {
+            var q1 = new[] { new DummyEntity { Id = 1 }, new DummyEntity { Id = 0 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 3 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var list = composable
+                .Select(i => i.Id)
+                .OrderBy(i => i)
+                .ToList();
+
+            Assert.That(list, Is.EqualTo(new[] { 0, 1, 2, 3 }));
+        }
+    }
+
+    class DummyEntity
+    {
+        public int Id { get; set; }
+    }
+}
