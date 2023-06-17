a3732b69ea0050f861b357fad46ea3c28a3e3409
Tomas Madajevas <tomas.madajevas@live.com>
2023-06-17 09:44:07 +0300

    reorder methods, add fiew more tests, mark failing as explicit

--- Various/Collections/Composite/CompositeQueryable/CompositeQueryProvider.cs
+++ Various/Collections/Composite/CompositeQueryable/CompositeQueryProvider.cs
@@ -5,70 +5,71 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Various.Collections.Composite.CompositeQueryable
+namespace Various.Collections.Composite.CompositeQueryable;
+
+internal class CompositeQueryProvider<T> : IQueryProvider
 {
-    internal class CompositeQueryProvider<T> : IQueryProvider
+    private readonly IQueryable<T>[] innerQueries;
+
+    public CompositeQueryProvider(IQueryable<T>[] innerQueries)
     {
-        private readonly IQueryable<T>[] innerQueries;
+        this.innerQueries = innerQueries;
+    }
 
-        public CompositeQueryProvider(IQueryable<T>[] innerQueries)
-        {
-            this.innerQueries = innerQueries;
-        }
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+    {
+        var queries = GetQueryables<TElement>(expression).ToArray();
 
-        public IQueryable CreateQuery(Expression expression)
+        return new CompositeQueryable<TElement>(queries);
+    }
+
+    private IEnumerable<IQueryable<TElement>> GetQueryables<TElement>(Expression expression)
+    {
+        if (expression is not MethodCallExpression methodCallExpression)
         {
             throw new NotImplementedException();
         }
+        var arguments = methodCallExpression.Arguments;
 
-        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        IEnumerable<Expression> GetArguments(IQueryable<T> inner)
         {
-            var queries = GetQueryables<TElement>(expression).ToArray();
-
-            return new CompositeQueryable<TElement>(queries);
-        }
-
-        private IEnumerable<IQueryable<TElement>> GetQueryables<TElement>(Expression expression)
-        {
-            // TODO: other types?
-            var methodCallExpression = expression as MethodCallExpression;
-            var arguments = methodCallExpression.Arguments;
-
-            IEnumerable<Expression> GetArguments(IQueryable<T> inner)
+            for (var i = 0; i < arguments.Count; i++)
             {
-                for (var i = 0; i < arguments.Count; i++)
+                var arg = arguments[i];
+
+                if (arg is ConstantExpression constExpr && constExpr.Type == typeof(CompositeQueryable<T>))
                 {
-                    var arg = arguments[i];
-
-                    if (arg is ConstantExpression constExpr && constExpr.Type == typeof(CompositeQueryable<T>))
-                    {
-                        yield return Expression.Constant(inner);
-                        continue;
-                    }
-
-                    yield return arg;
+                    yield return Expression.Constant(inner);
+                    continue;
                 }
-            }
 
-            foreach (var query in innerQueries)
-            {
-                var argsDebug = GetArguments(query).ToList();
-                var body = Expression.Call(null, methodCallExpression.Method, argsDebug);
-
-                var newQueryable = (IQueryable<TElement>)query.Provider.Execute(body);
-
-                yield return newQueryable;
+                yield return arg;
             }
         }
 
-        public object? Execute(Expression expression)
+        foreach (var query in innerQueries)
         {
-            throw new NotImplementedException();
-        }
+            var argsDebug = GetArguments(query).ToList();
+            var body = Expression.Call(null, methodCallExpression.Method, argsDebug);
 
-        public TResult Execute<TResult>(Expression expression)
-        {
-            throw new NotImplementedException();
+            var newQueryable = (IQueryable<TElement>)query.Provider.Execute(body);
+
+            yield return newQueryable;
         }
+    }
+
+    public IQueryable CreateQuery(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object? Execute(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TResult Execute<TResult>(Expression expression)
+    {
+        throw new NotImplementedException();
     }
 }
--- VariousTests/Collections/Composite/CompositeQueryable/CompositeQueryableTests.cs
+++ VariousTests/Collections/Composite/CompositeQueryable/CompositeQueryableTests.cs
@@ -35,7 +35,7 @@ namespace VariousTests.Collections.Composite.CompositeQueryable
             Assert.That(list, Is.EqualTo(new[] { 0, 1, 1, 2 }));
         }
 
-        [Test]
+        [Test, Explicit]
         public void ProvidedWithCollectionsContainingDuplicates_CanFilterThoseOut()
         {
             var q1 = new[] { new DummyEntity { Id = 0 }, new DummyEntity { Id = 1 } }.BuildMock();
@@ -50,11 +50,36 @@ namespace VariousTests.Collections.Composite.CompositeQueryable
             Assert.That(list, Is.EqualTo(new[] { 0, 1, 2 })); // nope. only works on single underlying collection
         }
 
+        [Test, Explicit]
+        public void SkipTakeOperations_WorksOnQueryablesAsAWhole()
+        {
+            var q1 = new[] { new DummyEntity { Id = 0 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 2 }, new DummyEntity { Id = 3 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var list = composable.Skip(1).Take(2).Select(i => i.Id).ToList();
+
+            Assert.That(list, Is.EqualTo(new[] { 1, 2 }));  // nope. gets second item of each queryable
+        }
+
+        [Test, Explicit]
+        public void GroupBy_CanGroupItemsOfMultipleQueryables()
+        {
+            var q1 = new[] { new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 1 } }.BuildMock();
+            var composable = new CompositeQueryable<DummyEntity>(q1, q2);
+
+            var grouped = composable.GroupBy(i => i.Id).ToList();
+
+            Assert.That(grouped.Count, Is.EqualTo(1)); // nope. items in collections are grouped separately
+        }
+
+
         [Test]
         public void ProvidedWithUnorderedItemsInBothCollections_CanOrderThem()
         {
             var q1 = new[] { new DummyEntity { Id = 1 }, new DummyEntity { Id = 0 } }.BuildMock();
-            var q2 = new[] { new DummyEntity { Id = 3 }, new DummyEntity { Id = 1 } }.BuildMock();
+            var q2 = new[] { new DummyEntity { Id = 3 }, new DummyEntity { Id = 2 } }.BuildMock();
             var composable = new CompositeQueryable<DummyEntity>(q1, q2);
 
             var list = composable
