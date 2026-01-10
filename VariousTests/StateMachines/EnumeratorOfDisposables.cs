0a0addd2b9755e5832455b32d75aaed3769a26fa
Tomas Madajevas <tomas.madajevas@live.com>
2026-01-10 09:35:47 +0200

    testing generators and disposables

---
+++ VariousTests/StateMachines/EnumeratorOfDisposables.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace VariousTests.StateMachines
+{
+    internal class EnumeratorOfDisposables : IEnumerable<IDisposable>, IEnumerator<IDisposable>
+    {
+        private const short INITIAL = 0;
+        private const short SECOND = 1;
+        private const short CLEAN  = 2;
+
+        private short state;
+
+        public EnumeratorOfDisposables() : this(-1) { }
+
+        private EnumeratorOfDisposables(short state)
+        {
+            this.state = state;
+        }
+
+        public IDisposable Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator<IDisposable> GetEnumerator()
+        {
+            return new EnumeratorOfDisposables(INITIAL);
+        }
+
+        public bool MoveNext()
+        {
+            switch (state)
+            {
+                case INITIAL:
+                    Current = Substitute.For<IDisposable>();
+                    state = SECOND;
+                    return true;
+                case SECOND:
+                    Current.Dispose();
+                    Current = Substitute.For<IDisposable>();
+                    state = CLEAN;
+                    return true;
+                case CLEAN:
+                    Current.Dispose();
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            Current?.Dispose();
+        }
+
+        public void Reset() => throw new NotSupportedException();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ VariousTests/StateMachines/ManualVsGeneratedTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VariousTests.StateMachines
+{
+    internal class ManualVsGeneratedTests
+    {
+        [Test]
+        public void ManualImplementation_ProducesCorrectBehaviour()
+        {
+            List<IDisposable> disposables = new List<IDisposable>();
+
+            var enumerable = new EnumeratorOfDisposables();
+            using var enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                disposables.Add(enumerator.Current);
+            }
+
+            Assert.That(disposables.Count, Is.EqualTo(2));
+            foreach (var disposable in disposables)
+            {
+                disposable.Received().Dispose();
+            }
+        }
+
+        [Test]
+        public void ManualImplementation_WhenConsumingCodeThrows_StillDisposesOfCreatedResources()
+        {
+            IDisposable disposable = null!;
+
+            try
+            {
+                var enumerable = new EnumeratorOfDisposables();
+                using var enumerator = enumerable.GetEnumerator();
+                enumerator.MoveNext();
+                disposable = enumerator.Current;
+                throw new Exception("Failed to process in some way");
+            }
+            catch { }
+
+            disposable.Received().Dispose();
+        }
+    }
+}
