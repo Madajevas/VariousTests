41e194a4926fdd50bd97d6f30bdbc64002f2f46d
Tomas Madajevas <tomas.madajevas@live.com>
2026-01-14 19:47:17 +0200

    randomize buffer sizes in tests

--- VariousTests/Streams/Base64StreamTests.cs
+++ VariousTests/Streams/Base64StreamTests.cs
@@ -26,7 +26,7 @@ namespace VariousTests.Streams
         }
 
         [Test]
-        public void Encoding_UsingWriteMethodWithOffsetAndLength_ProducesCorrectResult()
+        public void Encoding_UsingWriteMethodWithOffsetAndLength_ProducesCorrectResult([Random(1024, 4096, 7)] int bytesToWrite)
         {
             var bytes = new byte[64 * 1024 + 1];
             Random.Shared.NextBytes(bytes);
@@ -38,10 +38,10 @@ namespace VariousTests.Streams
             var offset = 0;
             while (offset < bytes.Length)
             {
-                var bytesCount = Math.Min(4096, bytes.Length - offset);
+                var bytesCount = Math.Min(bytesToWrite, bytes.Length - offset);
                 var buff = bytes[offset..(offset + bytesCount)];
                 base64Stream.Write(buff, 0, bytesCount);
-                offset += 4096;
+                offset += bytesToWrite;
             }
             base64Stream.Flush();
             var streamResult = Encoding.ASCII.GetString(output.ToArray());
@@ -75,11 +75,37 @@ namespace VariousTests.Streams
             }
             source.Position = 0;
 
-            var base64Stream = Base64Stream.CreateForDecoding(source);
+            using var base64Stream = Base64Stream.CreateForDecoding(source);
             using var target = new MemoryStream();
             base64Stream.CopyTo(target);
 
             Assert.That(target.ToArray(), Is.EqualTo(bytes));
+        }
+
+        [Test]
+        public void Decoding_UsingReadMethodWithOffsetAndLength_ProducesCorrectResult([Random(1024, 4096, 7)] int bytesToRead)
+        {
+            var bytes = new byte[64 * 1024 + 1];
+            Random.Shared.NextBytes(bytes);
+            var regularResult = Convert.ToBase64String(bytes);
+            var base64 = Convert.ToBase64String(bytes);
+            using var source = new MemoryStream();
+            using (var writer = new StreamWriter(source, leaveOpen: true))
+            {
+                writer.Write(base64);
+            }
+            source.Position = 0;
+
+            var decoded = new List<byte>();
+            using var base64Stream = Base64Stream.CreateForDecoding(source);
+            var buffer = new byte[bytesToRead];
+            int read;
+            while ((read = base64Stream.Read(buffer, 0, bytesToRead)) > 0)
+            {
+                decoded.AddRange(buffer.Take(read));
+            }
+
+            Assert.That(decoded, Is.EqualTo(bytes));
         }
     }
 }
