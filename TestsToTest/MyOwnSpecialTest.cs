cffe39c98745af3af3b72176a0e1fe67dde1769b
Tomas Madajevas <tomas.madajevas@live.com>
2026-01-27 21:41:00 +0200

    first draft of source generated dependant tests

---
+++ TestsToTest/MyOwnSpecialTest.cs
@@ -0,0 +1,35 @@
+﻿namespace TestsToTest
+{
+    [Multistep]
+    public partial class MyOwnSpecialTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [MyTest]
+        public int Test1()
+        {
+            return 42;
+        }
+
+        [MyTest]
+        public void Test2(int outputOfTestBefore)
+        {
+            Assert.That(outputOfTestBefore, Is.EqualTo(42));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class MultistepAttribute : Attribute
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class MyTestAttribute : Attribute
+    {
+
+    }
+}
