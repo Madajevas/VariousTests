088a2f3f63a095ca45246d13bd8279643dd2f0bd
Tomas Madajevas <tomas.madajevas@live.com>
2026-02-02 13:15:49 +0200

    add generator tests project

---
+++ TestsGenerator.Tests/DiagnosticsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TestsGenerator.Tests
+{
+    public class DiagnosticsTests
+    {
+        [Test]
+        public async Task Generator_ReportsDiagnosticsOnMissingParameters()
+        {
+            var source = """
+                using TestsGenerator.Abstractions;
+
+                [Multistep]
+                internal partial class MissingParamTest
+                {
+                    [MultistepParticipant]
+                    public void MissingParam(int missing) {}
+                }
+                """;
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+            IEnumerable<PortableExecutableReference> references = new[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+            };
+            CSharpCompilation compilation = CSharpCompilation.Create(assemblyName: "Tests", new[] { syntaxTree }, references);
+
+            var generator = new CustomSourceGenerator();
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+            driver = driver.RunGenerators(compilation);
+
+            var results = driver.GetRunResult();
+
+            await Verifier.Verify(results)
+                .UseDirectory("Snapshots")
+                .ToTask();
+        }
+    }
+}
---
+++ TestsGenerator.Tests/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestsGenerator.Tests
+{
+    [SetUpFixture]
+    internal class Initializer
+    {
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            VerifySourceGenerators.Initialize();
+        }
+    }
+}
