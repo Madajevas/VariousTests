d4c4d3d2d0ac2aaec05300fc50dd8a7fd0535d5b
Tomas Madajevas <tomas.madajevas@live.com>
2026-02-08 09:19:02 +0200

    move generator to folder

--- TestsGenerator.Abstractions/MultistepParticipantAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TestsGenerator.Abstractions
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class MultistepParticipantAttribute : Attribute
-    {
-    }
-}
